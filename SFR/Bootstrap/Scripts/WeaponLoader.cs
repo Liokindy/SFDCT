782a9c4ed16f92b1aec374b6b942f777f91d928d
Liokindy <59853852+Liokindy@users.noreply.github.com>
2025-01-16 23:42:27 -0300

    Animations and CTScripts
    - SFD Animations in a custom format, slightly different than SFR
    - Scripts folder, base files and an example

---
+++ SFR/Bootstrap/Scripts/WeaponLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFD.Weapons;
+using HarmonyLib;
+
+namespace SFDCT.Bootstrap.Scripts
+{
+    [HarmonyPatch]
+    internal static class WeaponLoader
+    {
+    }
+}
