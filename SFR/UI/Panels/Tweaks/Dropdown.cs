911a646554f4f4fc6654a8762aeab5f66170871d
Liokindy <59853852+Liokindy@users.noreply.github.com>
2025-01-19 21:17:42 -0300

    Dropdown Tweaks
    Dropdowns should have a corrected height (few items, shorter height, more items, larger height)

---
+++ SFR/UI/Panels/Tweaks/Dropdown.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SFD;
+using SFD.MenuControls;
+using HarmonyLib;
+
+namespace SFDCT.UI.Panels.Tweaks
+{
+    [HarmonyPatch]
+    internal static class Dropdown
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(MenuItemDropdown), MethodType.Constructor, new Type[] { typeof(string), typeof(string[]) })]
+        private static void MenuItemDropdownConstructor(MenuItemDropdown __instance)
+        {
+            if (__instance.values != null && __instance.values.Length > 0)
+            {
+                int length = __instance.values.Length;
+                float maxHeight = __instance.ParentMenu != null ? __instance.ParentMenu.Height : GameSFD.GAME_HEIGHT * 0.4f;
+
+                while (length * Menu.ITEM_HEIGHT > maxHeight && length > 3)
+                {
+                    length--;
+                }
+                
+                __instance.DropdownItemVisibleCount = length;
+            }
+        }
+    }
+}
